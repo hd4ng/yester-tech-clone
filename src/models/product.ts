66165d11695cd7f4b3aaf286a8b02e722664ca2f
Huy Dang <huy.dang@opsramp.com>
2020-03-31 22:15:18 -0700

    Add User and Product type

---
+++ src/models/product.ts
@@ -0,0 +1,29 @@
+export type Product = {
+  id: number
+  name: string
+  description: string
+  imagePath: string
+  inventory: number
+  price: number
+  year: string
+  condition: Condition
+  brand: Brand
+  category: Category
+  relatedProducts: number[]
+  rating: number
+}
+
+type Category = "Games" | "Gadgets" | "Music" | "Storage" | "Toys" | "Computers"
+type Brand =
+  | "Nintendo"
+  | "Google"
+  | "Rio"
+  | "Sony"
+  | "Iomega"
+  | "Bellmark"
+  | "Hasbro"
+  | "Sawyer’s"
+  | "Apple"
+  | "Emmett Brown Custom Manufacturing"
+  | "Epic Records."
+type Condition = "Excellent" | "Good" | "Fair" | "Poor"
