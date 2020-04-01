3203d5d56abfb64c6500afec723d33afb2847c52
Huy Dang <huy.dang@opsramp.com>
2020-03-31 22:15:39 -0700

    Create AuthStateContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react"
+import { BrowserRouter } from "react-router-dom"
 import "./styles/global-styles.scss"
 
 const App = () => {
-  return <div>Hello this is App component</div>
+  return (
+    <BrowserRouter>
+      <div>Hello this is App component</div>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ src/AuthState.tsx
@@ -0,0 +1,69 @@
+import React, {
+  Reducer,
+  Dispatch,
+  createContext,
+  useReducer,
+  useCallback,
+  useContext
+} from "react"
+import { User } from "./models"
+
+type AuthState = {
+  authenticated: boolean
+  user: User
+}
+
+type AuthAction =
+  | {
+      type: "LOGIN"
+      authenticated: boolean
+      user: User
+    }
+  | { type: "LOGOUT" }
+
+const initialState: AuthState = {
+  authenticated: false,
+  user: {} as User
+}
+
+type IAuthStateContext = AuthState & {
+  dispatch: Dispatch<AuthAction>
+}
+
+const AuthStateContext = createContext<IAuthStateContext | undefined>(undefined)
+
+export const AuthStateProvider: React.FC = ({ children }) => {
+  const [state, dispatch] = useReducer<Reducer<AuthState, AuthAction>>(
+    (state, action) => {
+      switch (action.type) {
+        case "LOGIN": {
+          return { ...state, authenticated: true, user: action.user }
+        }
+        case "LOGOUT": {
+          return initialState
+        }
+        default:
+          return state
+      }
+    },
+    initialState
+  )
+
+  const value = { ...state, dispatch: useCallback(dispatch, []) }
+
+  return (
+    <AuthStateContext.Provider value={value}>
+      {children}
+    </AuthStateContext.Provider>
+  )
+}
+
+export const useAuthState = () => {
+  const result = useContext(AuthStateContext)
+
+  if (result === undefined) {
+    throw new Error("useAuthState must be used within AuthStateProvider")
+  }
+
+  return result
+}
