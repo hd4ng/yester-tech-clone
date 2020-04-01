86296257f79040f051af5a46e9b755c4cde85973
Huy Dang <huy.dang@opsramp.com>
2020-03-31 21:39:06 -0700

    Create api utils

---
+++ src/api/utils.ts
@@ -0,0 +1,19 @@
+const baseURL = "http//localhost:3333"
+
+export function get(path: string) {
+  return fetch(`${baseURL}${path}`).then(res => res.json())
+}
+
+export function getRaw(path: string) {
+  return fetch(`${baseURL}${path}`)
+}
+
+export function post(path: string, data: { [key: string]: any } = {}) {
+  return fetch(`${baseURL}${path}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => res.json())
+}
