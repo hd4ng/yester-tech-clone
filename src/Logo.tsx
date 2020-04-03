d68f7b181721294a342b0c204f124f0545ad4147
Huy Dang <hdang.log@gmail.com>
2020-04-02 21:08:42 -0700

    Create Logo

---
+++ src/Logo.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { GoVersions } from "react-icons/go"
+import "./Logo.scss"
+
+const Logo = () => (
+  <span className="logo vertical-middle">
+    <GoVersions />{" "}
+    <span>
+      Yester<strong>Tech</strong>
+    </span>
+  </span>
+)
+
+export default Logo
--- src/PrimaryHeader.tsx
+++ src/PrimaryHeader.tsx
@@ -1,9 +1,21 @@
 import React from "react"
 import { Columns, Column } from "react-flex-columns"
 import "./PrimaryHeader.scss"
+import { Link } from "react-router-dom"
+import Logo from "./Logo"
 
 const PrimaryHeader = () => {
-  return <div className="primary-header"></div>
+  return (
+    <div className="primary-header">
+      <Columns gutters middle>
+        <Column flex>
+          <Link to="/" className="nav-logo">
+            <Logo />
+          </Link>
+        </Column>
+      </Columns>
+    </div>
+  )
 }
 
 export default PrimaryHeader
