803db24dae1df75ee7823ea498b857f815dc95f2
Huy Dang <hdang.log@gmail.com>
2020-04-18 21:56:36 -0700

    Implement ProductImage

---
+++ src/ProductImage.tsx
@@ -0,0 +1,25 @@
+import React from "react"
+import classnames from "classnames"
+
+import "./ProductImage.scss"
+
+type ProductImageProps = { size: number } & React.ImgHTMLAttributes<
+  HTMLImageElement
+>
+
+const ProductImage: React.FC<ProductImageProps> = ({
+  size = 7,
+  className,
+  ...rest
+}) => {
+  return (
+    <img
+      className={classnames("product-image", className)}
+      style={{ fontSize: `${size}em` }}
+      alt={rest.alt || ""}
+      {...rest}
+    />
+  )
+}
+
+export default ProductImage
